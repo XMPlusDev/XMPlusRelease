68e16894af5726ab135b5060d6ca69c7d02e2b8a
xcode75 <87862554+xcode75@users.noreply.github.com>
2026-03-15 12:12:28 +0000

    Update init.sh

--- init.sh
+++ init.sh
@@ -222,7 +222,7 @@ services:
     restart: unless-stopped
     volumes:
       - ./.env:/app/.env
-      - ./xmplus.log:/app/storage/logs/xmplus.log
+      - ./logs:/app/storage/logs
     healthcheck:
       test: ["CMD-SHELL", "curl -f http://127.0.0.1:9000/status"]
       interval: 30s
