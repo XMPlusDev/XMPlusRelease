2be0f535ffb80c8a15219449eee22c62e766a3e2
xcode75 <87862554+xcode75@users.noreply.github.com>
2026-06-13 00:52:36 +0000

    Update script-xmpanel.sh

--- script-xmpanel.sh
+++ script-xmpanel.sh
@@ -13,7 +13,6 @@ info()  { echo -e "${BLUE}[i]${NC} $1"; }
 error() { echo -e "${RED}[✘]${NC} $1"; }
 
 INSTALL_PATH="/usr/bin/XMPanel"
-SYMLINK_PATH="/usr/bin/xmpanel"
 SCRIPT_URL="https://raw.githubusercontent.com/XMPlusDev/XMPlusRelease/scripts/XMPanel.sh"
 
 # Check root
@@ -62,11 +61,6 @@ if [[ -f "$INSTALL_PATH" ]]; then
   rm -rf "$INSTALL_PATH"
 fi
 
-if [[ -f "$SYMLINK_PATH" ]]; then
-  info "Removing existing XMPanel symlink file..."
-  rm -rf "$SYMLINK_PATH"
-fi
-
 # Download
 info "Downloading XMPanel Script..."
 curl -o "$INSTALL_PATH" -Ls "$SCRIPT_URL"
@@ -74,8 +68,5 @@ log "Downloaded to $INSTALL_PATH"
 
 # Set permissions & symlink
 chmod +x "$INSTALL_PATH"
-ln -s "$INSTALL_PATH" "$SYMLINK_PATH"
-chmod +x "$SYMLINK_PATH"
-log "Permissions set and symlink created at $SYMLINK_PATH"
 
 log "XMPanel Script installed successfully. Run with: xmpanel"
